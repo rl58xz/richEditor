849f0e033e235e811f6256e6a23ed07ef8b5815a
renmac <renliang1000@163.com>
2020-10-22 20:31:53 +0800

    添加编辑区域和工具栏，设置点击事件

--- src/Editor.ts
+++ src/Editor.ts
@@ -1,26 +1,84 @@
-
 interface EditorConfig{
-    height:number;
+    bold:string;
+    title:string[];
 }
 
 export class Editor{
 
-    root:HTMLElement;
-    config:EditorConfig = {
-        height:500
+    root:HTMLDivElement;
+    config = {
+        bold:{
+            title:'B',
+            command:'bold'
+        },
+        title:['h1','h2','h3','h4','h5','normal'],
     };
 
+    //初始化
     constructor(root:string){
-        this.root = document.getElementById(root);
+        //设置挂载div边框
+        this.root = document.getElementById(root) as HTMLDivElement;
+        this.root.style.border = '1px solid #eee';
+        //增加全局样式
+        let style:HTMLStyleElement = document.createElement('style');
+        style.innerHTML = '.toolsitem:hover{background-color:#ddd}'
+        document.head.appendChild(style);
     }
 
     create(){
-        let toolsBar = document.createElement('div')
+        //创建文档片段
+        let df:DocumentFragment = new DocumentFragment();
+
+        //工具条创建
+        let toolsBar:HTMLDivElement = document.createElement('div');
         toolsBar.id = 'toolsbar';
-        let editorArea = document.createElement('div')
-        editorArea.id = 'editorarea';
-        editorArea.style.height = ''+this.config.height+'px';
-        this.root.appendChild(toolsBar);
-        this.root.appendChild(editorArea);
+        toolsBar.style.borderBottom = '1px solid #eee'
+        for(let key of Object.keys(this.config)){
+            let item = document.createElement('div');
+            item.classList.add('toolsitem');
+            item.classList.add('editorfundition')
+            item.style.display = 'inline-block';
+            item.style.height = 'inherit';
+            item.style.lineHeight = '2.5em';
+            item.style.width = '2.5em';
+            item.style.textAlign = 'center';
+            item.textContent = String(this.config[key].title);
+            item.dataset.command = this.config[key].command;
+            if(this.config[key].params) item.dataset.params = this.config[key].params;
+            else item.dataset.params = null;
+            // if(this.config[key] instanceof Array){
+            //     let subMenu = document.createElement('ul');
+            //     for(let liitem of this.config[key]){
+            //         let subMenuItem = document.createElement('li');
+            //         subMenuItem.textContent = String(liitem);
+            //         subMenu.appendChild(subMenuItem);
+            //     }
+            //     item.appendChild(subMenu);
+            // }
+            toolsBar.appendChild(item);
+        }
+
+        //编辑区域创建
+        let editorArea:HTMLDivElement = document.createElement('div');
+        editorArea.setAttribute('id',"editorarea");
+        editorArea.contentEditable = 'true';
+
+        //挂载到页面
+        df.appendChild(toolsBar);
+        df.appendChild(editorArea);
+        this.root.appendChild(df);
+
+        //设置点击事件
+        let funditionButtons:HTMLCollection = document.getElementsByClassName('editorfundition');
+        for(let i = 0; i < funditionButtons.length; i++){
+            let funditionButton:HTMLElement = funditionButtons[i] as HTMLElement;
+            funditionButton.addEventListener('click',()=>{
+                if(window.getSelection){
+                    let range = window.getSelection();
+                    console.log(range);
+                }
+                document.execCommand(funditionButton.dataset.command,false,funditionButton.dataset.params);
+            })
+        }
     }
 }
